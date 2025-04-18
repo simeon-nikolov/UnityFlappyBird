77b04ecf7fa2ce928dee429fb34aaa72bfa9018b
Simeon Nikolov <simeon.v.nikolov@gmail.com>
2025-04-18 17:18:23 +0300

    Add Bird and Pipes

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D birdRigidbody;
+    public float flapStrength = 12;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.birdRigidbody.linearVelocity = Vector2.up * this.flapStrength;
+        }
+    }
+}
